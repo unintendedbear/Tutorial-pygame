3ede2645b27b8679ac70c8e61b19504b41bc3c3f
unintendedbear <unintendedbear@gmail.com>
2016-10-20 16:47:44 +0200

    Basic template for pygame, imports and initialises pygame.

---
+++ example1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""example1.py"""
+
+# Import pygame
+import pygame
+from pygame.locals import *
+
+# Define main
+def main():
+    return 0
+
+# Initialise pygame 
+if __name__ == '__main__':
+    pygame.init()
+    main()
