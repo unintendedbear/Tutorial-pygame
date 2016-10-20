df5e2ba00df82207ee4c7e660ae156f441a5466c
unintendedbear <unintendedbear@gmail.com>
2016-10-20 17:07:27 +0200

    Creating my first window :')

--- example1.py
+++ example1.py
@@ -5,12 +5,25 @@
 # Import pygame
 import pygame
 from pygame.locals import *
+import sys
+
+# Window width and height, they'll be constants for now
+WIDTH = 640
+HEIGHT = 480
 
 # Define main
 def main():
+    # Creating screen
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Testing testing!")
+    # Maintains screen opened unless manually closed
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
     return 0
 
-# Initialise pygame 
+# Initialise pygame
 if __name__ == '__main__':
     pygame.init()
     main()
