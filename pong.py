58755bfe072f60d527461e0ed47bfca1aaf6558b
unintendedbear <unintendedbear@gmail.com>
2016-10-21 12:06:58 +0200

    The ball moves!

--- pong.py
+++ pong.py
@@ -28,6 +28,17 @@ class Ball(pygame.sprite.Sprite):
         self.rect.centery = HEIGHT / 2
         self.speed = [0.5, -0.5]
 
+    # Controling ball movement
+    def refresh(self, time):
+        self.rect.centerx += self.speed[0] * time # Basic physics
+        self.rect.centery += self.speed[1] * time
+        if self.rect.left <= 0 or self.rect.right >= WIDTH:
+            self.speed[0] = -self.speed[0]
+            self.rect.centerx += self.speed[0] * time
+        if self.rect.top <= 0 or self.rect.bottom >= HEIGHT:
+            self.speed[1] = -self.speed[1]
+            self.rect.centery += self.speed[1] * time
+
 # Function which loads an image into the Window
 def load_image(path, transparent):
     try: image = pygame.image.load(path)
@@ -48,16 +59,21 @@ def main():
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Unintended Pong")
 
+    # Graphic elements
     background = load_image(backgrnd_path, False)
-
     ball = Ball()
+
+    # Creating clock so the ball moves
+    clock = pygame.time.Clock()
 
     # Maintains screen opened unless manually closed
     while True:
+        time = clock.tick(60)
         for event in pygame.event.get():
             if event.type == QUIT:
                 sys.exit()
 
+        ball.refresh(time)
         screen.blit(background, (0, 0))
         screen.blit(ball.image, ball.rect)
         pygame.display.flip()
