6a88ca63f557dc37d17f7eec1e5b040fade0959c
unintendedbear <unintendedbear@gmail.com>
2016-10-21 11:42:33 +0200

    Yay! By creating an object call Ball, now I have a ball in the game table :D

--- pong.py
+++ pong.py
@@ -12,7 +12,21 @@ WIDTH = 640
 HEIGHT = 480
 
 # Path for the background image
-image_path = "images/background_pong.png"
+backgrnd_path = "images/background_pong.png"
+
+# Path for the background image
+ball_path = "images/ball.png"
+
+# Ball object
+class Ball(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = load_image(ball_path, True)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2 # Ball will appear in the center of the background
+        self.rect.centery = HEIGHT / 2
+        self.speed = [0.5, -0.5]
 
 # Function which loads an image into the Window
 def load_image(path, transparent):
@@ -34,7 +48,9 @@ def main():
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Unintended Pong")
 
-    background = load_image(image_path, False)
+    background = load_image(backgrnd_path, False)
+
+    ball = Ball()
 
     # Maintains screen opened unless manually closed
     while True:
@@ -43,6 +59,7 @@ def main():
                 sys.exit()
 
         screen.blit(background, (0, 0))
+        screen.blit(ball.image, ball.rect)
         pygame.display.flip()
     return 0
 
